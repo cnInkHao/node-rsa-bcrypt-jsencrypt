cddfd42db483b8a2a719bb2418ab45b35cf12d79
873219794@qq.com <family867868>
2020-03-18 21:47:32 +0800

    存入数据库的密码不可逆 使用hash加salt加密 （bcrypt）

--- nodeRsa/decryptJSEncrypt.js
+++ nodeRsa/decryptJSEncrypt.js
@@ -17,3 +17,21 @@ decryptJSEncrypt()
 
 // 运行
 // node nodeRsa/decryptJSEncrypt.js
+
+
+// 存入数据库的密码不可逆 使用hash加salt加密 （bcrypt）
+
+// const bcrypt = require('bcrypt');
+// // 前端加密后的密码
+// const pwd = 'Jp/S6Bd/CR6PeEJstoUXD5fRj5FWjoRqEzwJc+qgF1iz4kBCvtRdnrbosvBZ7pd2eCN3z0zvJFbfcaj9APG7Ew=='
+// // bcrypt.hash(pwd, 10, function(err, hash) {
+// //     // Store hash in your password DB.
+// //     console.log(hash)
+// // });
+// // 生成的hash密码并存入数据库
+// const hash = '$2b$10$pBk0Rueiqx7hRIcEVuohFOSHPsToQDrOmG4S3iGpMJrAc6JGkVXoe'
+
+// bcrypt.compare(pwd, hash, function(err, result) {
+//     // result == true
+//     console.log('密码验证', result)
+// });
